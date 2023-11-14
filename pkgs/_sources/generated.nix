3c702af1f717b7876fac08809201b1368d0d956c
the-argus <the-argus@users.noreply.github.com>
2023-11-14 18:33:07 +0000

    Bump Spicetify Packages

--- pkgs/_sources/generated.nix
+++ pkgs/_sources/generated.nix
@@ -55,16 +55,16 @@
   };
   comfySrc = {
     pname = "comfySrc";
-    version = "1209fd23cbbb665c2d4fdf7dd1c2642f2cc45396";
+    version = "51c4a339af0c020e3e3bdb4b0700ab677ef9d0d4";
     src = fetchgit {
       url = "https://github.com/Comfy-Themes/Spicetify";
-      rev = "1209fd23cbbb665c2d4fdf7dd1c2642f2cc45396";
+      rev = "51c4a339af0c020e3e3bdb4b0700ab677ef9d0d4";
       fetchSubmodules = false;
       deepClone = false;
       leaveDotGit = false;
-      sha256 = "sha256-6yGKvSCeoJ9HSNWPnscehaxek6xw2hGefeq59+ce7Zs=";
+      sha256 = "sha256-DXF9DVzUns4+egezB1MynqpwprLJ+fmK1j5RmZkj7v8=";
     };
-    date = "2023-11-12";
+    date = "2023-11-14";
   };
   customAppsExtensionsSrc = {
     pname = "customAppsExtensionsSrc";
